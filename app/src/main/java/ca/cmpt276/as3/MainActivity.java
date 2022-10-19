398b0a69ff0968fb308638106db25f9fa824de92
shadmanhossain <shadmanh@sfu.ca>
2022-10-19 00:24:12 -0700

    Added background to welcome screen with welcome message.
    Added background and welcome message through activity_main.xml.

--- app/src/main/java/ca/cmpt276/as3/MainActivity.java
+++ app/src/main/java/ca/cmpt276/as3/MainActivity.java
@@ -5,16 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView welcomeMessage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        welcomeMessage = findViewById(R.id.tvWelcomeMessage);
+        startAnimation();
         setUpSkipButton();
-
     }
 
 
@@ -24,5 +29,8 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
+    public void startAnimation(){
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.animation);
+        welcomeMessage.startAnimation(animation );
+    }
 }
