85acc0668094fc49b9031e5586e608189c1fa1dd
shadmanhossain <shadmanh@sfu.ca>
2022-10-19 18:18:10 -0700

    Animated Welcome Message
    Added zoom_in and zoom_out animations

--- app/src/main/java/ca/cmpt276/as3/MainActivity.java
+++ app/src/main/java/ca/cmpt276/as3/MainActivity.java
@@ -1,30 +1,21 @@
 package ca.cmpt276.as3;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
-import android.widget.TextView;
-
-import ca.cmpt276.as3.model.WelcomeMessageFragment;
 
 public class MainActivity extends AppCompatActivity {
-    TextView welcomeMessage;
+    Dialog dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
-        welcomeMessage = findViewById(R.id.tvWelcomeMessage);
-        FragmentManager manager = getSupportFragmentManager();
-        WelcomeMessageFragment dialog = new WelcomeMessageFragment();
-        dialog.show(manager, "Message Dialog");
-        //startAnimation();
+        dialog = new Dialog(MainActivity.this);
+        startAnimation();
         setUpSkipButton();
     }
 
@@ -36,7 +27,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startAnimation(){
-        Animation animation = AnimationUtils.loadAnimation(this,R.anim.animation);
-        welcomeMessage.startAnimation(animation );
+        dialog.setContentView(R.layout.welcome_message);
+        dialog.getWindow().setWindowAnimations(R.style.AnimationsForDialog);
+        dialog.show();
     }
 }
