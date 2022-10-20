b47f3110ea19df0a9090f9f423ae7342d2b04634
shadmanhossain <shadmanh@sfu.ca>
2022-10-20 15:57:45 -0700

    Updated Help Screen and Backgrounds
    Added background used in welcome screen to all other screens. Updated the help screen to explain how to play the game.

--- app/src/main/java/ca/cmpt276/as3/MainActivity.java
+++ app/src/main/java/ca/cmpt276/as3/MainActivity.java
@@ -2,21 +2,35 @@ package ca.cmpt276.as3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
     Dialog dialog;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
         dialog = new Dialog(MainActivity.this);
-        startAnimation();
         setUpSkipButton();
+        startAnimation();
+        //TODO: Make activity change 4 seconds after animation is over
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, MainMenu.class));
+            }
+        }, 8000);
+
     }
 
 
